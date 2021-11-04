0715ecd0d48b42676e565e511d2d13a32fa3ee82
onuri5 <jomibaofficial@gmail.com>
2021-11-04 18:07:47 -0500

    Migrations set up

---
+++ data/migrations/20211104223310_first_migration.js
@@ -0,0 +1,37 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (table) => {
+      table.increments("project_id");
+      table.string("project_name").notNullable();
+      table.string("project_description");
+      table.boolean("project_completed").defaultTo(false);
+    })
+
+    .createTable("resources", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name").notNullable().unique();
+      table.string("resource_description");
+    })
+
+    .createTable("tasks", (table) => {
+      table.increments("task_id");
+      table.string("resource_description").notNullable();
+      table.string("task_notes");
+      table.boolean("task_completed").defaultTo(false);
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks");
+};
